d2dadc4aec4f14f30d5cfad2ed0d20dde1f691fa
Ranjan Verma <verma.ranjan62@gmail.com>
2022-08-24 15:49:53 +0530

    Into to Middleware

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
-
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// app.use(logger);
+
+//dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcamps);
 
