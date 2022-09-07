916887eb85b42772b1b852b277ab86f0da84a2bd
Ranjan Verma <verma.ranjan62@gmail.com>
2022-09-07 15:48:40 +0530

    Forgot Password Generate

--- controllers/auth.js
+++ controllers/auth.js
@@ -90,3 +90,25 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     data: user,
   });
 });
+
+// @desc   Forgot Password
+//@route  GET /api/v1/auth/forgotPassword
+// @access Public
+
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  //Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
